834e30edfd05a2d98cc3d03356b7005e5ea98014
gleysii <gleysibianchi@gmail.com>
2023-01-23 01:14:43 -0300

    adição do click para overlay

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-//selecionando 
+//selecionando - habilidades
 const cardList = document.querySelectorAll('.card');//nodeList
 const descriptionList = document.querySelectorAll('.description');//nodeList
 const field = document.querySelector('.dynamic-text');
+//selecionando - galeria/squares
+const squareList = document.querySelectorAll('.square');
+const overlayList = document.querySelectorAll('.overlay');
 
+//percorrendo cardList
 for(let i = 0; i < cardList.length; i++){
     //elementos recebem seus índices individualmente
     const card = cardList[i];
@@ -11,7 +15,19 @@ for(let i = 0; i < cardList.length; i++){
     card.addEventListener("click", () => {
         field.innerHTML = description.innerHTML;
     });
-    
 }
+
+//percorrendo squareList
+for(let i = 0; i < squareList.length; i++){
+    const square = squareList[i];
+    const overlay = overlayList[i];
+
+    square.addEventListener("click", () =>{
+        overlay.style.opacity = 0.7;
+    })
+
+}
+
+
 
 
